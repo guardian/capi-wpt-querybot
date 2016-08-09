bc6515178dfc15d4d7e7b8bec86424a902ee6f1f
Michael McNamara <michael.mcnamara@guardian.co.uk>
2016-08-09 13:05:45 +0100

    more text

--- src/main/scala/app/App.scala
+++ src/main/scala/app/App.scala
@@ -686,7 +686,8 @@ object App {
     println(listOfDupes.length + " Duplicate test results found")
     println("length of sorted By Weight Combined List is: " + combinedListLength)
     println("length of errorFreeSortedByWeightCombinedResults: " + errorFreeCombinedListLength)
-    println("length of elements list in first result: " + sortedByWeightCombinedResults.head.fullElementList.length)
+    println("length of elements list in first result in combinedResultsList: " + combinedResultsList.head.fullElementList.length)
+    println("length of elements list in first result in sortedByWeightCombinedResults: " + sortedByWeightCombinedResults.head.fullElementList.length)
   }
 
   def getResultPages(urlList: List[String], urlFragments: List[String], wptBaseUrl: String, wptApiKey: String, wptLocation: String): List[(String, String)] = {
