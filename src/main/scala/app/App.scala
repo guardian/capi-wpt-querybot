032aeecb345e00659bbbc7102449b2c3121d44e2
Michael McNamara <michael.mcnamara@theguardian.com>
2016-08-15 15:48:52 +0100

    Changing runlog file to reduce number of logs

--- src/main/scala/app/App.scala
+++ src/main/scala/app/App.scala
@@ -74,10 +74,10 @@ object App {
     // summary files
 
     val jobStartHour = jobStart.hourOfDay.getAsString
-    val jobStartDayOfMonth = jobStart.dayOfMonth.getAsString
-    val runSummaryFile = "runLogs/runSummary" + jobStartDayOfMonth + jobStartHour + ".txt"
-    val pageWeightAlertSummaryFile = "runLogs/pageWeightAlertSummary" + jobStartDayOfMonth + jobStartHour + ".txt"
-    val interactiveAlertSummaryFile = "runLogs/interactiveAlertSummary" + jobStartDayOfMonth + jobStartHour + ".txt"
+    val jobStartDayOfWeek = jobStart.dayOfWeek.getAsString
+    val runSummaryFile = "runLogs/runSummary" + jobStartDayOfWeek + "_" + jobStartHour + ".txt"
+    val pageWeightAlertSummaryFile = "runLogs/pageWeightAlertSummary" + jobStartDayOfWeek + "_" + jobStartHour + ".txt"
+    val interactiveAlertSummaryFile = "runLogs/interactiveAlertSummary" + jobStartDayOfWeek + "_" + jobStartHour + ".txt"
 
     //Define colors to be used for average values, warnings and alerts
     val averageColor: String = "#d9edf7"
