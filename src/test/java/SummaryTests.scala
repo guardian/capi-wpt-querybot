ffb7a5e62d0633393dd8d02b80f3bd5e997e64c4
Michael McNamara <michael.mcnamara@guardian.co.uk>
2016-08-25 14:32:21 +0100

    filter out glabs content from pageweight alerts

--- src/test/java/SummaryTests.scala
+++ src/test/java/SummaryTests.scala
@@ -1,6 +1,6 @@
 
   import app.HtmlReportBuilder
-  import app.api.{SummaryPage, DataSummary, JSONOperations, S3Operations}
+  import app.api._
   import app.apiutils._
   import com.gu.contentapi.client.model.v1.{MembershipTier, Office, ContentFields, CapiDateTime}
   import org.joda.time.DateTime
@@ -46,6 +46,7 @@
     val visualsPagesFileName = "visuals.conf"
 
     val resultsFromPreviousTests = "resultsFromPreviousTests.csv"
+    //val resultsFromPreviousTests = "resultFromPreviousTestsAmalgamated.csv"
     // val resultsFromPreviousTests = "resultsFromPreviousTestsShortened.csv"
     //val resultsFromPreviousTests = "shortenedresultstest.csv"
     val outputFile = "shortenedresultstest.csv"
@@ -94,29 +95,161 @@
     val testResultsHandler = new ResultsFromPreviousTests(previousResults)
     println("\n\n\n ***** There are " + testResultsHandler.previousResults.length + " previous results in file  ********* \n\n\n")
 
-    val dataSummary = new DataSummary(time1HourAgo, currentTime, 10, 20, emptyPerfResults,testResultsHandler)
+    val dataSummary = new DataSummary(time1HourAgo, currentTime, 10, 20, emptyPerfResults, testResultsHandler)
     //write summaries to files
     val runSummaryFile = "runSummaryStringtest.txt"
     val runSummaryHTMLFile = "runSummaryHTMLStringtest.html"
 
 
+    "Element summary" should "be returned as string" in {
+      val elementSummary = dataSummary.summaryList.head
+      val dataSummaryString = dataSummary.returnElementSummaryAsHTMLString(elementSummary)
+      println("\n\n\n Single summary: \n" + dataSummaryString)
+    }
 
-      "Element summary" should "be returned as string" in {
-        val elementSummary = dataSummary.summaryList.head
-        val dataSummaryString = dataSummary.returnElementSummaryAsHTMLString(elementSummary)
-        println("\n\n\n Single summary: \n" + dataSummaryString)
+    "Data Summary object " should " be able to produce a data summary from the results object" in {
+      println("writing run summary data to new file")
+      val summaryPage = new SummaryPage(dataSummary)
+      val summaryPageHTMLString = summaryPage.toString()
+      s3Interface.writeFileToS3(runSummaryFile, dataSummary.summaryDataToString())
+      s3Interface.writeFileToS3(runSummaryHTMLFile, summaryPageHTMLString)
+      dataSummary.printSummaryDataToScreen()
+      assert(true)
+    }
+
+    /*"Not a test PageElementSamples page " should "be populated and display correctly when I run this" in {
+      val resultsFromAlertList = "alerts/pageWeightAlertsFromPreviousTests.csv"
+      val alertList = s3Interface.getResultsFileFromS3(resultsFromAlertList)
+      val alertHandler = new ResultsFromPreviousTests(alertList)
+      println("about to make alertHandler")
+      val alertSummary = new DataSummary(time1HourAgo, currentTime, 10, 20, emptyPerfResults, alertHandler)
+      println("alertHandler build ok")
+      val samplePage = new PageElementSamples(dataSummary, alertSummary)
+      s3Interface.writeFileToS3("pageElementSamplePages.html", samplePage.toString())
+      assert(true)
+    }*/
+
+/*    "Not a test but I " should "be able to get a list of pages with an example of each embed" in {
+
+      def getPage(pageList: List[PerformanceResultsObject]): String = {
+        if (pageList.nonEmpty) {
+          pageList.head.testUrl
+        } else {
+          "coming soon"
+        }
       }
 
-      "Data Summary object " should " be able to produce a data summary from the results object" in {
-        println("writing run summary data to new file")
-        val summaryPage = new SummaryPage(dataSummary)
-        val summaryPageHTMLString = summaryPage.toString()
-        s3Interface.writeFileToS3(runSummaryFile, dataSummary.summaryDataToString())
-        s3Interface.writeFileToS3(runSummaryHTMLFile, summaryPageHTMLString)
-        dataSummary.printSummaryDataToScreen()
-        assert(true)
-      }
+      val audioboom: String = getPage(dataSummary.pagesWithAudioBoomEmbed)
+      val brightcove: String = getPage(dataSummary.pagesWithBrightcoveEmbed)
+      val cnn: String = getPage(dataSummary.pagesWithCNNEmbed)
+      val dailymotion: String = getPage(dataSummary.pagesWithDailyMotionEmbed)
+      val documentCloud: String = getPage(dataSummary.pagesWithDocumentCloudEmbed)
+      val facebook: String = getPage(dataSummary.pagesWithFacebookEmbed)
+      val formStack: String = getPage(dataSummary.pagesWithFormStackEmbed)
+      val gif: String = getPage(dataSummary.pagesWithGifEmbed)
+      val googleMaps: String = getPage(dataSummary.pagesWithGoogleMapsEmbed)
+      val guardianAudio: String = getPage(dataSummary.pagesWithGuardianAudio)
+      val guardianComments: String = getPage(dataSummary.pagesWithGuardianCommentsEmbed)
+      val guardianVideo: String = getPage(dataSummary.pagesWithGuardianVideos)
+      val guardianImages: String = getPage(dataSummary.pagesWithGuardianImages)
+      val guardianUpload: String = getPage(dataSummary.pagesWithGuardianUpload)
+      val guardianWitnessImage: String = getPage(dataSummary.pagesWithGuardianWitnessImageEmbed)
+      val guardianWitnessVideo: String = getPage(dataSummary.pagesWithGuardianWitnessVideoEmbed)
+      val hulu: String = getPage(dataSummary.pagesWithHuluEmbed)
+      val image: String = getPage(dataSummary.pagesWithImageEmbed)
+      val infoStrada: String = getPage(dataSummary.pagesWithInfoStradaEmbed)
+      val instagram: String = getPage(dataSummary.pagesWithInstagramEmbed)
+      val interactive: String = getPage(dataSummary.pagesWithInteractiveEmbed)
+      val mp3: String = getPage(dataSummary.pagesWithMP3Embed)
+      val mp4: String = getPage(dataSummary.pagesWithMP4Embed)
+      val parliamentLiveTv: String = getPage(dataSummary.pagesWithParliamentLiveTvEmbed)
+      val scribd: String = getPage(dataSummary.pagesWithScribdEmbed)
+      val soundCloud: String = getPage(dataSummary.pagesWithSoundCloudEmbed)
+      val spotify: String = getPage(dataSummary.pagesWithSpotifyEmbed)
+      val twitter: String = getPage(dataSummary.pagesWithTwitterEmbed)
+      val uStream: String = getPage(dataSummary.pagesWithUStreamEmbed)
+      val vevo: String = getPage(dataSummary.pagesWithVevoEmbed)
+      val vimeo: String = getPage(dataSummary.pagesWithVimeoEmbed)
+      val vine: String = getPage(dataSummary.pagesWithVineEmbed)
+      val youTube: String = getPage(dataSummary.pagesWithYouTubeEmbed)
+      val unknownEmbed: String = getPage(dataSummary.pagesWithUnknownEmbed)
 
+      val stringArray = Array(
+        audioboom,
+        brightcove,
+        cnn,
+        dailymotion,
+        documentCloud,
+        facebook,
+        formStack,
+        gif,
+        googleMaps,
+        guardianAudio,
+        guardianComments,
+        guardianVideo,
+        guardianImages,
+        guardianUpload,
+        guardianWitnessImage,
+        guardianWitnessVideo,
+        hulu,
+        image,
+        infoStrada,
+        instagram,
+        interactive,
+        mp3,
+        mp4,
+        parliamentLiveTv,
+        scribd,
+        soundCloud,
+        spotify,
+        twitter,
+        uStream,
+        vevo,
+        vimeo,
+        vine,
+        youTube,
+        unknownEmbed
+      )
+
+      val output: String =
+        "audioboom: " + stringArray(0) + "\n" +
+          "brightcove: " + stringArray(1) + "\n" +
+          "cnn: " + stringArray(2) + "\n" +
+          "dailymotion: " + stringArray(3) + "\n" +
+          "documentCloud: " + stringArray(4) + "\n" +
+          "facebook: " + stringArray(5) + "\n" +
+          "formStack: " + stringArray(6) + "\n" +
+          "gif: " + stringArray(7) + "\n" +
+          "googleMaps: " + stringArray(8) + "\n" +
+          "guardianAudio: " + stringArray(9) + "\n" +
+          "guardianComments: " + stringArray(10) + "\n" +
+          "guardianVideo: " + stringArray(11) + "\n" +
+          "guardianImages: " + stringArray(12) + "\n" +
+          "guardianUpload: " + stringArray(13) + "\n" +
+          "guardianWitnessImage: " + stringArray(14) + "\n" +
+          "guardianWitnessVideo: " + stringArray(15) + "\n" +
+          "hulu: " + stringArray(16) + "\n" +
+          "image: " + stringArray(17) + "\n" +
+          "infoStrada: " + stringArray(18) + "\n" +
+          "instagram: " + stringArray(19) + "\n" +
+          "interactive: " + stringArray(20) + "\n" +
+          "mp3: " + stringArray(21) + "\n" +
+          "mp4: " + stringArray(22) + "\n" +
+          "parliamentLiveTv: " + stringArray(23) + "\n" +
+          "scribd: " + stringArray(24) + "\n" +
+          "soundCloud: " + stringArray(25) + "\n" +
+          "spotify: " + stringArray(26) + "\n" +
+          "twitter: " + stringArray(27) + "\n" +
+          "uStream: " + stringArray(28) + "\n" +
+          "vevo: " + stringArray(29) + "\n" +
+          "vimeo: " + stringArray(30) + "\n" +
+          "vine: " + stringArray(31) + "\n" +
+          "youTube: " + stringArray(32) + "\n" +
+          "unknownEmbed: " + stringArray(33) + "\n"
+
+      s3Interface.writeFileToS3("samplePagesForEmbedsFromAlerts.txt", "Sample Pages for each embed type: \n \n" + output)
+      assert(true)
+    }*/
 
   }
 
