6f00bc5c69daa0e14613a9a7b24f65bcd940a5c8
Michael McNamara <michael.mcnamara@guardian.co.uk>
2016-09-06 17:10:06 +0100

    reverting name change

--- src/main/scala/app/api-utils/ResultsFromPreviousTests.scala
+++ src/main/scala/app/api-utils/ResultsFromPreviousTests.scala
@@ -15,12 +15,12 @@ class ResultsFromPreviousTests(resultsList: List[PerformanceResultsObject]) {
   //val fullResultsList = resultsList
 
   val cutoffTime: Long = DateTime.now.minusHours(24).getMillis
-  val allResults: List[PerformanceResultsObject] = resultsList
+  val previousResults: List[PerformanceResultsObject] = resultsList
   //val previousResults: List[PerformanceResultsObject] = resultsList
 
-  val resultsFromLast24Hours = for (result <- allResults if result.mostRecentUpdate >= cutoffTime) yield result
-  val oldResults = for (result <- allResults if result.mostRecentUpdate < cutoffTime) yield result
-  val oldestResult = allResults.last
+  val resultsFromLast24Hours = for (result <- previousResults if result.mostRecentUpdate >= cutoffTime) yield result
+  val oldResults = for (result <- previousResults if result.mostRecentUpdate < cutoffTime) yield result
+  val oldestResult = previousResults.last
 
   val previousResultsToRetest: List[PerformanceResultsObject] = for (result <- resultsFromLast24Hours if result.needsRetest()) yield result
   val recentButNoRetestRequired: List[PerformanceResultsObject] = for (result <- resultsFromLast24Hours if !result.needsRetest()) yield result
@@ -38,10 +38,10 @@ class ResultsFromPreviousTests(resultsList: List[PerformanceResultsObject]) {
   val timeOfOldestTest = oldestResult.timeOfTest
 
   def returnPagesNotYetTested(list: List[(Option[ContentFields], Seq[Tag], String)]): List[(Option[ContentFields], Seq[Tag], String)] = {
-    val pagesNotYetTested: List[(Option[ContentFields], Seq[Tag], String)] = for (page <- list if !allResults.map(_.testUrl).contains(page._3)) yield page
-    val pagesAlreadyTested:List[(Option[ContentFields], Seq[Tag], String)] = for (page <- list if allResults.map(_.testUrl).contains(page._3)) yield page
+    val pagesNotYetTested: List[(Option[ContentFields], Seq[Tag], String)] = for (page <- list if !previousResults.map(_.testUrl).contains(page._3)) yield page
+    val pagesAlreadyTested:List[(Option[ContentFields], Seq[Tag], String)] = for (page <- list if previousResults.map(_.testUrl).contains(page._3)) yield page
     val testedPagesBothSourcesThatHaveChangedSinceLastTest = pagesAlreadyTested.flatMap(page => {
-      for (result <- allResults if result.testUrl.contains(page._3) && result.mostRecentUpdate < page._1.get.lastModified.getOrElse(new CapiDateTime {
+      for (result <- previousResults if result.testUrl.contains(page._3) && result.mostRecentUpdate < page._1.get.lastModified.getOrElse(new CapiDateTime {
         override def dateTime: Long = 0
       }).dateTime) yield page}).distinct
    // println("pages that have been updated since last test: \n" + testedPagesBothSourcesThatHaveChangedSinceLastTest.map(_._2 + "\n").mkString)
@@ -108,11 +108,11 @@ class ResultsFromPreviousTests(resultsList: List[PerformanceResultsObject]) {
 
   //query full list
   def returnAllPageWeightAlerts(): List[PerformanceResultsObject] = {
-    allResults.filter(_.alertStatusPageWeight)
+    previousResults.filter(_.alertStatusPageWeight)
   }
 
   def returnAllPageSpeedAlerts(): List[PerformanceResultsObject] = {
-    allResults.filter(_.alertStatusPageSpeed)
+    previousResults.filter(_.alertStatusPageSpeed)
   }
 
   def returnPageSpeedAlertsWithinSizeLimits(): List[PerformanceResultsObject] = {
@@ -122,23 +122,23 @@ class ResultsFromPreviousTests(resultsList: List[PerformanceResultsObject]) {
 
 
   def checkConsistency(): Boolean = {
-        if (!(((previousResultsToRetest.length + recentButNoRetestRequired.length) == resultsFromLast24Hours.length) && ((resultsFromLast24Hours.length + oldResults.length) == allResults.length))) {
+        if (!(((previousResultsToRetest.length + recentButNoRetestRequired.length) == resultsFromLast24Hours.length) && ((resultsFromLast24Hours.length + oldResults.length) == previousResults.length))) {
           println("ERROR: previous results list handling is borked!")
           println("Previous Results to retest length == " + previousResultsToRetest.length + "\n")
           println("Unchanged previous results length == " + recentButNoRetestRequired.length + "\n")
           println("Results from last 24 hours length == " + resultsFromLast24Hours.length + "\n")
           println("Old results length == " + oldResults.length + "\n")
-          println("Original list of previous results length == " + allResults.length + "\n")
+          println("Original list of previous results length == " + previousResults.length + "\n")
           if (!((previousResultsToRetest.length + recentButNoRetestRequired.length) == resultsFromLast24Hours.length)) {
             println("Results to test and unchanged results from last 24 hours dont add up correctly \n")
           }
-          if (!((resultsFromLast24Hours.length + oldResults.length) == allResults.length)) {
+          if (!((resultsFromLast24Hours.length + oldResults.length) == previousResults.length)) {
             println("Results from last 24 hours and old results dont add up \n")
           }
           false
         } else {
           println("Retrieved results from file\n")
-          println(allResults.length + " results retrieved in total")
+          println(previousResults.length + " results retrieved in total")
           println(resultsFromLast24Hours.length + " results for last 24 hours")
           println(previousResultsToRetest.length + " results will be elegible for retest")
           println(dedupedPreviousResultsToRestest.length + " results are not duplicates and will actually be retested")
@@ -265,7 +265,7 @@ class ResultsFromPreviousTests(resultsList: List[PerformanceResultsObject]) {
 
 
   def countResultsWithNoElements(): Int = {
-    allResults.count(_.editorialElementList.isEmpty)
+    previousResults.count(_.editorialElementList.isEmpty)
   }
 
   def reAddPageElementsToPastResults(): List[PerformanceResultsObject] = {
@@ -291,7 +291,7 @@ class ResultsFromPreviousTests(resultsList: List[PerformanceResultsObject]) {
     val wptLocation: String = configArray(3)
 
 
-    val repairedResultsList = allResults.map(result => {if(result.editorialElementList.isEmpty) {
+    val repairedResultsList = previousResults.map(result => {if(result.editorialElementList.isEmpty) {
       val newResult = try {
         getResult(result.testUrl, result.friendlyResultUrl, wptBaseUrl, wptApiKey, urlFragments)
       }
